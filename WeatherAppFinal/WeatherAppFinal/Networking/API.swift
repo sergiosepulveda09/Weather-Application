ccbd6d258fd03bf5bce1d5b1dd554a7b3b174e21
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-05-06 13:51:05 -0700

    Add files via upload

---
+++ WeatherAppFinal/WeatherAppFinal/Networking/API.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct API {
+    static let key = "963c32d2914b277c766419f2e819db81"
+}
---
+++ WeatherAppFinal/WeatherAppFinal/WeatherAppFinalApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct WeatherAppFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
